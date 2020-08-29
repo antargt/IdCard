4debc1c79c5e58072bcf1df4aa76d2f250045bbe
Anthony Aragon <aragonanthonyj@gmail.com>
2020-08-28 22:02:45 -0700

    Final
    Final project submitted
    minor clean programming issues
    need to comment out

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ function idCard() {
   var lastName = document.getElementById("lname").value;
   var mailAddress = document.getElementById("address").value;
   var fullName = firstName + " " + lastName;
+  var age = parseInt(document.getElementById("age").value);
+  var phoneNumber = parseInt(document.getElementById("telephone").value);
+  var numberArray = [];
   
+  numberArray.push(age, phoneNumber);
+    
+  for (var i = 0; i < numberArray.length; i++) {
+	if (numberArray[i] <= 100) {
+	  document.getElementById("postAge").innerHTML = "Age: " + age;
+	} else if (numberArray[i] > 100) {
+	  document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + phoneNumber;    	  
+    }   
+  }
   
   document.getElementById("postFullName").innerHTML = fullName;
-  document.getElementById("postAge").innerHTML = "Age: " + document.getElementById("age").value;
-  document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + document.getElementById("telephone").value;
   document.getElementById("postAddress").innerHTML = mailAddress;
 }
